7707b94ab58dd9182b4901542bd71341dcc51017
Lukasz Stafiniak <lukstafi@gmail.com>
2023-10-26 16:29:46 +0200

    The TeXmacs style I created for syntax highlighting -- `highlight.sty` is its export to LaTeX.

---
+++ for_texmacs/highlight.ts
@@ -0,0 +1,49 @@
+<TeXmacs|1.0.7.14>
+
+<style|source>
+
+<\body>
+  <assign|hlstd|<macro|codetext|<verbatim|<with|color|#000000|<arg|codetext>>>>>
+
+  <assign|hlnum|<macro|codetext|<verbatim|<with|color|#000040|<arg|codetext>>>>>
+
+  <assign|hlesc|<macro|codetext|<verbatim|<with|color|#BC8C8C|<arg|codetext>>>>>
+
+  <assign|hlstr|<macro|codetext|<verbatim|<with|color|#BC8C8C|<arg|codetext>>>>>
+
+  <assign|hlpps|<macro|codetext|<verbatim|<with|color|#BC8C8C|<arg|codetext>>>>>
+
+  <assign|hlslc|<macro|codetext|<verbatim|<with|color|#AA2121|<arg|codetext>>>>>
+
+  <assign|hlcom|<macro|codetext|<verbatim|<with|color|#AA2121|<arg|codetext>>>>>
+
+  <assign|hlppc|<macro|codetext|<verbatim|<with|color|#000000|<arg|codetext>>>>>
+
+  <assign|hlopt|<macro|codetext|<verbatim|<with|color|#404060|<arg|codetext>>>>>
+
+  <assign|hllin|<macro|codetext|<verbatim|<with|color|#545454|<arg|codetext>>>>>
+
+  <assign|hlkwa|<macro|codetext|<verbatim|<with|color|#9B21ED|<arg|codetext>>>>>
+
+  <assign|hlkwb|<macro|codetext|<verbatim|<with|color|#218921|<arg|codetext>>>>>
+
+  <assign|hlkwc|<macro|codetext|<verbatim|<with|color|#0000FF|<arg|codetext>>>>>
+
+  <assign|hlkwd|<macro|codetext|<verbatim|<with|color|#000000|<arg|codetext>>>>>
+
+  <assign|textunderscore|<macro|_>>
+
+  <assign|textasciicircum|<macro|^>>
+
+  <assign|hlline|<\macro|codeline>
+    <arg|codeline><line-break>
+  </macro>>
+
+  <assign|hlendline|<macro|comment|<htab|5mm><small|<arg|comment>>>>
+</body>
+
+<\initial>
+  <\collection>
+    <associate|language|american>
+  </collection>
+</initial>
